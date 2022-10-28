6810fba3dfda61b18c8fae6f66ca0b34244bead6
Anshika1610 <71184320+Anshika1610@users.noreply.github.com>
2022-10-28 14:25:22 +0530

    Create hurdle4.py

---
+++ hurdle4.py
@@ -0,0 +1,36 @@
+def turn_right():
+   turn_left()
+   turn_left()
+   turn_left() 
+    
+def blockjump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+def wall():
+    if wall_in_front():
+        turn_left()
+        move()
+        while wall_on_right():
+            move()   
+        turn_right()
+        move()
+               
+        turn_right()
+        move()
+        while wall_on_right() and not wall_in_front():
+            move() 
+        turn_left()
+        
+    
+while not at_goal():
+    if wall_in_front():
+        wall()
+    else:
+        move()
+
